d91396df8b9b21547034f9902fffc52aa891ef20
Jason Yap <jason.yap@kp.org>
2018-11-06 19:03:00 -0800

    greet new user and alert others.

--- server/server.js
+++ server/server.js
@@ -17,7 +17,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected.');
 
+    //socket.emit from admin text welcome to the chat app
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
 
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+    //broadcast emit from admin text new user joined
 
     socket.on('disconnect', () => {
         console.log('User disconnected.');
@@ -30,6 +42,11 @@ io.on('connection', (socket) => {
             text: data.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: data.from,
+        //     text: data.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 });
 
